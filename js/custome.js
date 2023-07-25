5477fc18456eed94b1f9ac2c521560ef6fe6c5f5
Wise Peace <137463770+httpnayeemerahman@users.noreply.github.com>
2023-07-25 10:00:24 +0600

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    $('.teamslide').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false,
+      });
+          
+
+
+})
